b22881b79190e7dff2abc7487c3fb10d377a8137
Jay Dixit <sunjaydixit@gmail.com>
2015-06-04 23:33:25 -0400

    project complete

--- auto/jaykomaletter.el
+++ auto/jaykomaletter.el
@@ -5,14 +5,14 @@
                      '(("scrlttr2" "foldmarks=false" "refline=dateleft" "fromrule=afteraddress" "enlargefirstpage=on" "fontsize=12pt" "fromalign=center" "subject=left" "parskip=full")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "English") ("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "scrlttr2"
